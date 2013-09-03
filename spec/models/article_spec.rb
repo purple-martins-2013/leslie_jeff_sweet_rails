d7c4f082611c2d30dc13543ab5d8c48121a3d806
DBC Apprentice <apprentice@dbc.com>
2013-09-03 15:17:47 -0700

    Create model tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,4 +1,22 @@
 require 'spec_helper'
 
 describe Article do
-  
+  let(:article) { Article.new(title: "Cool Article") }
+  it { should belong_to(:category) }
+  it { should have_and_belong_to_many(:tags) }
+
+  context "it sets a url before creating an object" do
+
+    it "does not have a url before creation" do
+      expect(article.url).to be_nil
+    end
+
+    it "has a url after creation" do
+      article.save
+      time = Time.now
+      expect(article.url).to eq "articles/#{time.month}/#{time.day}/#{time.year}/cool-article"
+    end
+
+  end
+
+end

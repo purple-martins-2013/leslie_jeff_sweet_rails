e3057582ba2578a61a1d26218656c915fc790420
DBC Apprentice <apprentice@dbc.com>
2013-09-03 12:06:41 -0700

    Add redirect to new url format and show article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,9 +11,13 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    category_id = Category.find_by(name: params[:category_name].gsub("_", " ")).id
+    @article = Article.find_by(category_id: category_id, title: params[:title].gsub("-", " "))
   end
 
   def redirect
+    article = Article.find_by_url("articles/#{params[:month]}/#{params[:day]}/#{params[:year]}/#{params[:title]}")
+    redirect_to "/categories/#{article.category.name.gsub(" ", "_")}/articles/#{params[:title]}"
   end
 
 end

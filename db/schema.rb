f9c20fb0e3628fcb5ae5660f8a035e02646989a1
DBC Apprentice <apprentice@dbc.com>
2013-09-03 10:15:35 -0700

    add categories table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130311183138) do
+ActiveRecord::Schema.define(version: 20130903171448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(version: 20130311183138) do
     t.string "url"
     t.string "category"
     t.string "tags"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string "name"
   end
 
 end

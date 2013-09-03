7e0acbd8e2934310dd4ca60e3b6d19a3a3a87422
DBC Apprentice <apprentice@dbc.com>
2013-09-03 11:13:16 -0700

    deleted extraneous article_id column from tags

---
+++ db/migrate/20130903181100_delete_article_id_from_tags.rb
@@ -0,0 +1,5 @@
+class DeleteArticleIdFromTags < ActiveRecord::Migration
+  def change
+    remove_column :tags, :article_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130903180317) do
+ActiveRecord::Schema.define(version: 20130903181100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,10 +33,7 @@ ActiveRecord::Schema.define(version: 20130903180317) do
   end
 
   create_table "tags", force: true do |t|
-    t.string  "name"
-    t.integer "article_id"
+    t.string "name"
   end
-
-  add_index "tags", ["article_id"], name: "index_tags_on_article_id", using: :btree
 
 end

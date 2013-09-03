4ddd6ce514892eed70a5d69d42014d1c3810bb94
DBC Apprentice <apprentice@dbc.com>
2013-09-03 10:35:27 -0700

    add tags table, again

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130903171448) do
+ActiveRecord::Schema.define(version: 20130903173337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

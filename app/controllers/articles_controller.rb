33f9e81bec4b521002a87f33fa25ab326b006809
DBC Apprentice <apprentice@dbc.com>
2013-09-03 11:45:25 -0700

    build out article controller and view for index

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,19 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def redirect
+  end
+
+end

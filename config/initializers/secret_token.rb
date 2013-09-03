d2537ad534e9dd144a7127be4a2adfb40d5e105e
DBC Apprentice <apprentice@dbc.com>
2013-09-03 10:05:25 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SweetRails::Application.config.secret_key_base = '4ec40ebbd4cd1f4860d6aabd43f9192019e31926f3346cb7fe4c068a5e3737f0f1fb5eb091dd10ebc667de66102989e99b8c0c04ac102027dbf6d2d74c6a8828'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SweetRails::Application.config.session_store :cookie_store, key: '_sweet_rails_session'
